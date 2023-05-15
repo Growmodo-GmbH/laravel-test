bb00729101603c1551fb49f624f28759f3b63b19
Mark Gabinete <justinpaul1025@gmail.com>
2023-05-15 12:28:13 +0800

    test authentication with breeze

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+export default {
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+    ],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
+    css: {
+        postcss: {
+          plugins: [
+            require('autoprefixer'),
+            require('postcss-import'),
+            require('tailwindcss'),
+          ],
+        },
+    },
 });
