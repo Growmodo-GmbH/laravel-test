cc16003e6db20ff0f0c5780d31f0d1b256939b31
marlontamo <marlontamo@gmail.com>
2023-06-06 11:04:00 +0800

    modified RouteServiceProvider, LoginController, RegisterController

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    public const WELCOME ='/welcome';
+
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
