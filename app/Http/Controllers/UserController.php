2625d394f72260970d690d43699e83d34a19184d
Johanes Peter Vincentius <JP20-2@slc.net>
2023-06-20 10:50:26 +0700

    feat: add delete and update endpoint for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,60 +13,26 @@ class UserController extends Controller
         $userList = User::with('role')->paginate(16);
         return response()->json($userList);
     }
-
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(User $user)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, User $user)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'phone' => ['required', 'numeric', 'digits:10',  'unique:' . User::class]
+        ]);
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->phone = $request->phone;
+
+        $user->save();
+
+        return response()->json(['message' => 'User updated successfully']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return response()->json(['message' => 'User deleted successfully']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::prefix('/users')->group(function () {
-        Route::get('', [UserController::class, 'all'])->middleware(IsAdmin::class);
-    });
+        Route::get('', [UserController::class, 'all']);
+        Route::delete('/{user}', [UserController::class, 'destroy']);
+        Route::put('/{user}', [UserController::class, 'update']);
+    })->middleware(IsAdmin::class);
 });
