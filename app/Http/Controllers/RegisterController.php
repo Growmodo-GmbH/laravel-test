131623f7381b1703c580246373e8a9ae597793ae
Daphne Del Rosario <dapdelrosario14@gmail.com>
2023-06-27 22:11:20 +0800

    Update register controller

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,7 +14,16 @@ class RegisterController extends Controller
     public function store(Request $request) {
         $input = $request->all();
 
-        // Create users information
+        $username = User::where('username', $request['username'])->first();;
+
+        $email = User::where('email', $request['email'])->first();;
+
+        if ($username) {
+            return response()->json(['status' => 409, 'message' => "Username already exist, please try again!"]);
+        } else if ($email) {
+            return response()->json(['status' => 409, 'message' => "Email address already exist, please try again!"]);
+        } else if(!$username && !$email) {
+            // Create users information
         User::create([
             'username' => $input['username'],
             'email' => $input['email'],
@@ -22,7 +31,8 @@ class RegisterController extends Controller
             'password' => Hash::make($input['password'])
         ]);
 
-        // Response with status and message
-        return response()->json(['status' => 200, 'message' => "User successfully added!"]);
+            // Response with status and message
+            return response()->json(['status' => 200, 'message' => "User successfully added!"]);
+        }
     }
 }
