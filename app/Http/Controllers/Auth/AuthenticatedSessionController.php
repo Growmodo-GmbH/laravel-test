320f9dff0fc7bb78246d0bbd78f2844a16a3896c
Johanes Peter Vincentius <JP20-2@slc.net>
2023-07-06 16:23:18 +0700

    feat: make auth logic for bearer token

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
@@ -15,19 +16,14 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
-        $request->session()->regenerate();
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token');
 
-        return response()->noContent();
+        return response(['auth-token' => $token->plainTextToken]);
     }
 
-    public function destroy(Request $request): Response
+    public function destroy(): Response
     {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
         return response()->noContent();
     }
 }
